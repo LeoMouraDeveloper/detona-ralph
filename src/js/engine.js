5712f9a3f968de76a9c06f980ddba75a7cbe15c8
Leonardo Moura <leonardomoura.santos25@gmail.com>
2024-10-28 14:55:04 -0300

    Correção no código

--- src/js/engine.js
+++ src/js/engine.js
@@ -29,7 +29,7 @@ const state = {
   }
   
   function playSound(audioName) {
-    let audio = new Audio(`./src/audios/${audioName}.m4a`);
+    let audio = new Audio(`./src/sounds/${audioName}.m4a`);
     audio.volume = 0.2;
     audio.play();
   }
